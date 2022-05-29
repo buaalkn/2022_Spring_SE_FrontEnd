64f96b239d4fe80fc6f00c9e59b4244ec9ae5dec
unique021203 <liuxinwendexiaowo@163.com>
2022-05-29 13:56:40 +0800

    修改登录/注册接口

--- YRMS/yrms/src/api/index.js
+++ YRMS/yrms/src/api/index.js
@@ -7,15 +7,15 @@ export const reqGetCode = (phone) => requests({ url: `http://127.0.0.1:4523/mock
 
 //注册
 //URL:/api/user/passport/register method:post	 phone code password
-export const reqUserRegister = (data) => requests({ url: '/user/passport/register', data, method: 'post' });
+export const reqUserRegister = (data) => requests({ url: 'http://127.0.0.1:4523/mock/1005801/user/passport/register', data, method: 'post' });
 
 //登录
 //URL:/api/user/passport/login method:post	 phone password
-export const reqUserLogin = (data) => requests({ url: '/user/passport/login', data, method: 'post' });
+export const reqUserLogin = (data) => requests({ url: 'http://127.0.0.1:4523/mock/1005801/user/passport/login', data, method: 'post' });
 
 //验证码登录
 //URL:/api/user/passport/loginvf method:post	 phone code
-export const reqUserLoginWithVfCode = (data) => requests({ url: '/user/passport/loginvf', data, method: 'post' });
+export const reqUserLoginWithVfCode = (data) => requests({ url: 'http://127.0.0.1:4523/mock/1005801/user/passport/loginvf', data, method: 'post' });
 
 
 //axios发请求返回结果是Promise对象
